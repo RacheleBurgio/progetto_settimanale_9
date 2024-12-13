72b4f8d7edde5a75eabb43abc56134e1c3320827
RacheleBurgio <rachele.burgio32@gmail.com>
2024-12-13 12:00:16 +0100

    aggiunti assets e creato mini layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import Nav from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Nav />
       </header>
-    </div>
+      <main>
+        <div className="App">
+          <div className="row g-0">
+            {/* Colonna più piccola */}
+            <div className="col-2">
+              <div style={{ backgroundColor: '#f8f9fa', height: '57em' }}>
+                Colonna piccola
+              </div>
+            </div>
+            {/* Colonna più grande */}
+            <div className="col-10">
+              <div style={{ backgroundColor: '#e9ecef', height: '57em' }}>
+                Colonna grande
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
 
